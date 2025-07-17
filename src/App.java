fa5601ee0c5ebe4c76ec9edf02f5b249f846946c
aru_segar <arunigagnanasegaran@gmail.com>
2025-07-17 15:09:27 +0530

    create the window of the game

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.JFrame;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) throws Exception { 
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("Pacman");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       }
 }
