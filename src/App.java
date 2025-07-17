403ffc7dede3eff340fc9bfb86b89bcd4110a6ce
aru_segar <arunigagnanasegaran@gmail.com>
2025-07-17 15:16:04 +0530

    create the jpanel

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 
 public class App {
-    public static void main(String[] args) throws Exception { 
+    public static void main(String[] args) throws Exception {
         int rowCount = 21;
         int columnCount = 19;
         int tileSize = 32;
@@ -9,10 +9,15 @@ public class App {
         int boardHeight = rowCount * tileSize;
 
         JFrame frame = new JFrame("Pacman");
-        frame.setVisible(true);
+        // frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       }
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
